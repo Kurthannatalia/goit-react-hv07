14656fc8c103f1f581e678711d7e62d4a3ab93d8
Natalia Kurthan <kurthannatalia@gmail.com>
2025-01-25 19:54:51 +0300

    upd

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,35 +1,70 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import { addContact, deleteContact, fetchContacts } from "./contactsOps";
+import { createSelector } from "@reduxjs/toolkit";
+import { selectContacts, selectNameFilter } from "./selectors";
 
+const handlePending = (state) => {
+  state.loading = true;
+};
+
+const handleError = (state, action) => {
+  state.loading = false;
+  state.error = action.payload;
+};
 const contactsSlice = createSlice({
   name: "contacts",
   initialState: {
-    items: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    items: [],
+    loading: false,
+    error: null,
   },
-  reducers: {
-    addContact: {
-      reducer(state, action) {
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchContacts.pending, handlePending)
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, handleError)
+      .addCase(addContact.pending, handlePending)
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
         state.items.push(action.payload);
-      },
-      prepare(name, number) {
-        return {
-          payload: {
-            id: nanoid(),
-            name,
-            number,
-          },
-        };
-      },
-    },
-    deleteContact(state, action) {
-      const index = state.items.findIndex((task) => task.id === action.payload);
-      state.items.splice(index, 1);
-    },
+      })
+      .addCase(addContact.rejected, handleError)
+      .addCase(deleteContact.pending, handlePending)
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        const index = state.items.findIndex(
+          (contact) => contact.id === action.payload.id
+        );
+        state.items.splice(index, 1);
+      })
+      .addCase(deleteContact.rejected, handleError);
   },
 });
-export const { addContact, deleteContact } = contactsSlice.actions;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectNameFilter],
+  (contacts, selectNameFilter) => {
+    return contacts.filter((contact) => {
+      if ("id" in contact && "name" in contact && "number" in contact) {
+        if (
+          typeof contact.id === "string" &&
+          typeof contact.name === "string" &&
+          typeof contact.number === "string"
+        ) {
+          return contact.name
+            .toLowerCase()
+            .includes(selectNameFilter.toLowerCase());
+        }
+      }
+      return false;
+    });
+  }
+);
+
 export const contactReducer = contactsSlice.reducer;
